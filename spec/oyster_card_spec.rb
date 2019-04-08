6b02379fb0c8aea2168ac3a21c3e00a1a2485d5e
sarpong <abasimisarpong@rocketmail.com>
2019-04-08 17:04:24 +0100

    all test pass

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -11,9 +11,13 @@ describe OysterCard do
   end
   describe '#top_up' do
     it 'tops the oyster card up by 10' do 
-      expect { subject.top_up(10) }.to change{ subject.balance }.from(0).to(10)
+      expect { subject.top_up(10)}.to change{ subject.balance }.by 10
+    end
+
+    it 'raises an error when card limit of 90 is reached' do
+      subject.top_up(90)
+      expect { subject.top_up(1)}.to raise_error 'card limit of 90 reached'
     end
   end
 end
-  
 
