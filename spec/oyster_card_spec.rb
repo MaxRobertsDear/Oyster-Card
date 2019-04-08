92adcecdb79ba02fe8d253285da15ee460860d7e
sarpong <abasimisarpong@rocketmail.com>
2019-04-08 22:33:39 +0100

    No test fails

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,6 +1,6 @@
 require 'oyster_card'
 
-describe OysterCard do 
+describe OysterCard do
   describe '#balance' do
     it 'responds to balance' do
       expect(subject).to respond_to :balance
@@ -10,14 +10,13 @@ describe OysterCard do
     end
   end
   describe '#top_up' do
-    it 'tops the oyster card up by 10' do 
+    it 'tops the oyster card up by 10' do
       expect { subject.top_up(10)}.to change{ subject.balance }.by 10
     end
 
     it 'raises an error when card limit of 90 is reached' do
       subject.top_up(90)
-      expect { subject.top_up(1)}.to raise_error 'card limit of 90 reached'
+      expect { subject.top_up(1)}.to raise_error 'card limit of £90 reached'
     end
   end
 end
-
