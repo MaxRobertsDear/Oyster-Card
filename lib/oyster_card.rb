7fdf2816efe5dd7c3736ff531144d0d8151ca992
sarpong <abasimisarpong@rocketmail.com>
2019-04-08 22:33:16 +0100

    fixed the top up method

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,21 +1,22 @@
 class OysterCard
-  
+
   CARD_LIMIT = 90
   attr_reader :balance
-  
-  def initialize 
+
+  def initialize
     @balance = 0
   end
 
   def top_up(input)
-    raise "card limit of #{ CARD_LIMIT } reached" if limit_exceeded? 
+    raise "card limit of £#{ CARD_LIMIT } reached" if limit_exceeded?(input)
     @balance += input
   end
 
   private
 
-  def limit_exceeded?
-    @balance >= CARD_LIMIT
+  def limit_exceeded?(input)
+    user_top_up = @balance + input
+    user_top_up > CARD_LIMIT
   end
 
 end
